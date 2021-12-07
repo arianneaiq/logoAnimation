17fa620e82de1423d7c45763aba2ddc3e72e7f06
arianneaiq <82588258+arianneaiq@users.noreply.github.com>
2021-12-07 14:20:11 +0100

    logo

--- animePoc.js
+++ animePoc.js
@@ -1,8 +1,34 @@
-let animation = anime({
-    targets: 'div',
-    translateX: 100,
-    borderRadius: 50,
-    duration: 2000,
-    easing: 'linear',
-    direction: 'alternate'
-  });  
+let pen = anime({
+    targets: 'transform',
+    left: '240px',
+    rotate: {
+      value: 800,
+      duration: 1500,
+      easing: 'easeOutQuad'
+    },
+  }); 
+
+let name = anime({
+  targets: '.demo',
+  translateX: {
+    value: 250,
+    duration: 800
+  },
+  rotate: {
+    value: 360,
+    duration: 1800,
+    easing: 'easeInOutSine'
+  },
+  scale: {
+    value: 2,
+    duration: 1600,
+    delay: 800,
+    easing: 'easeInOutQuart'
+  },
+  delay: 250 // All properties except 'scale' inherit 250ms delay
+});
+  
+
+
+
+  
